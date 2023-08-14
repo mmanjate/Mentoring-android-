db66fc60e03b53a92c1e1e6cae9c952ab8864c2b
Jose Julai Ritsure <ritsure.julai.jose@gmail.com>
2023-08-14 17:22:41 +0200

    DAO constructors from protected to public

--- app/src/main/java/mz/org/csaude/mentoring/model/location/Cabinet.java
+++ app/src/main/java/mz/org/csaude/mentoring/model/location/Cabinet.java
@@ -1,19 +1,25 @@
 package mz.org.csaude.mentoring.model.location;
 
+import com.j256.ormlite.field.DatabaseField;
+import com.j256.ormlite.table.DatabaseTable;
+import lombok.Data;
 import mz.org.csaude.mentoring.base.model.BaseModel;
+import mz.org.csaude.mentoring.dao.location.CabinetDAOImpl;
 
+@Data
+@DatabaseTable(tableName = Cabinet.TABLE_NAME, daoClass = CabinetDAOImpl.class)
 public class Cabinet extends BaseModel {
+    public static final String TABLE_NAME = "cabinet";
 
+    public static final String COLUMN_NAME = "name";
+
+    @DatabaseField(columnName = COLUMN_NAME, unique = true, canBeNull = false)
     private String name;
 
     public Cabinet() {
     }
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
+    public Cabinet(String name) {
         this.name = name;
     }
 }
--- app/src/main/java/mz/org/csaude/mentoring/model/location/District.java
+++ app/src/main/java/mz/org/csaude/mentoring/model/location/District.java
@@ -1,29 +1,32 @@
 package mz.org.csaude.mentoring.model.location;
 
+import com.j256.ormlite.field.DatabaseField;
+import com.j256.ormlite.table.DatabaseTable;
+import lombok.Data;
 import mz.org.csaude.mentoring.base.model.BaseModel;
+import mz.org.csaude.mentoring.dao.location.DistrictDAOImpl;
 
+@Data
+@DatabaseTable(tableName = District.TABLE_NAME, daoClass = DistrictDAOImpl.class)
 public class District extends BaseModel {
 
+    public static final String TABLE_NAME = "district";
+
+    public static final String COLUMN_PROVINCE = "province";
+
+    public static final String COLUMN_DISTRICT = "district";
+
+    @DatabaseField(columnName = COLUMN_PROVINCE, canBeNull = false)
     private Province province;
 
+    @DatabaseField(columnName = COLUMN_DISTRICT, unique = true, canBeNull = false)
     private String district;
 
     public District() {
     }
 
-    public Province getProvince() {
-        return province;
-    }
-
-    public String getDistrict() {
-        return district;
-    }
-
-    public void setProvince(Province province) {
+    public District(Province province, String district) {
         this.province = province;
-    }
-
-    public void setDistrict(String district) {
         this.district = district;
     }
 }

f92be0ae8e224b454d6b900cb903b08a2706b253
Jose Julai Ritsure <ritsure.julai.jose@gmail.com>
2024-07-03 14:02:12 +0200

    Manter Rondas - Upload e Sincronizacao - Sync time fix

---
+++ app/src/main/java/mz/org/csaude/mentoring/workSchedule/work/SessionWorker.java
@@ -0,0 +1,47 @@
+package mz.org.csaude.mentoring.workSchedule.work;
+
+import android.content.Context;
+
+import androidx.annotation.NonNull;
+import androidx.work.WorkerParameters;
+
+import java.sql.SQLException;
+import java.util.List;
+
+import mz.org.csaude.mentoring.base.worker.BaseWorker;
+import mz.org.csaude.mentoring.model.session.Session;
+
+public class SessionWorker extends BaseWorker<Session> {
+    private String requestType;
+
+    public SessionWorker(@NonNull Context context, @NonNull WorkerParameters workerParams) {
+        super(context, workerParams);
+        requestType = getInputData().getString("requestType");
+    }
+
+    @Override
+    public void doOnlineSearch(long offset, long limit) throws SQLException {
+        getApplication().getSessionRestService().restGetSessions(this);
+    }
+
+    @Override
+    protected void doOnStart() {
+
+    }
+
+    @Override
+    protected void doAfterSearch(String flag, List<Session> recs) throws SQLException {
+        changeStatusToFinished();
+        doOnFinish();
+    }
+
+    @Override
+    protected void doOnFinish() {
+
+    }
+
+    @Override
+    protected void doSave(List<Session> recs) {
+
+    }
+}
